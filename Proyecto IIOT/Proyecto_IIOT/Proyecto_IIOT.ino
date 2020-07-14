b5bba3895d96030db0835f7fb3a39dcddd16b20e
rubenavas23 <61756306+rubenavas23@users.noreply.github.com>
2020-07-14 16:41:50 +0200

    Add files via upload

--- Proyecto IIOT/Proyecto_IIOT/Proyecto_IIOT.ino
+++ Proyecto IIOT/Proyecto_IIOT/Proyecto_IIOT.ino
@@ -6,19 +6,19 @@
 #define LED_AZUL 16
 #define LED_ROJO 5
 #define LED_VERDE 15
-#define Motor 13
-#define Presencia 17
+#define MOTOR 13
+#define PRESENCIA 17
 #define DHT_PIN 21     // Defines pin number to which the sensor is connected
 #define DHT_TYPE DHT11 // Defines the sensor type. It can be DHT11 or DHT22
 // Define new subscription topics here
 #define COMMAND_TOPIC "rele"
-#define TEST_TOPIC "P"
+//#define TEST_TOPIC "P"
 
 // Replace the next variables with your Wi-Fi SSID/Password
-const char *WIFI_SSID = "MOVISTAR_A781";
-const char *WIFI_PASSWORD = "101214RubenCristina";
+const char *WIFI_SSID = "AndroidAP";
+const char *WIFI_PASSWORD = "plbh5028";
 char macAddress[18];
-bool Pres=LOW;
+bool pres=LOW;
 const char *MQTT_BROKER_IP = "iiot-upc.gleeze.com";
 const int MQTT_PORT = 1883;
 const char *MQTT_USER = "iiot-upc";
@@ -40,8 +40,8 @@ void setup() {
   pinMode(LED_AZUL, OUTPUT); // Pinout as output
   pinMode(LED_VERDE, OUTPUT); // Pinout as output
   pinMode(LED_ROJO, OUTPUT); // Pinout as output
-  pinMode(Motor, OUTPUT); // Pinout as output
-  pinMode(Presencia, INPUT); // Pinout as output
+  pinMode(MOTOR, OUTPUT); // Pinout as output
+  pinMode(PRESENCIA, INPUT); // Pinout as output
   
   //Comunicación MQTT
   mqttClient.setServer(MQTT_BROKER_IP,MQTT_PORT); // Connect the configured mqtt broker
@@ -63,11 +63,11 @@ void loop() {
   nowTime = millis();
   elapsedTime = nowTime - startTime;
   
-  SensPresencia();
+  sensPresencia();
   leds();
   if (elapsedTime >= 2000) {
     //publishIntNumber();   // Publishes an int number
-    LecturaDHT11();
+    lecturaDHT11();
     publishDHT11();     // Publishes a big json
     startTime = nowTime;
   }
@@ -75,7 +75,7 @@ void loop() {
 
 void leds(){
 
-  if (Pres==HIGH){
+  if (pres==HIGH){
     digitalWrite(LED_VERDE,LOW);
     digitalWrite(LED_AZUL,HIGH);
     digitalWrite(LED_ROJO,LOW);
@@ -89,18 +89,18 @@ void leds(){
 
 
 
-void SensPresencia(){
+void sensPresencia(){
   bool val;
-  val = digitalRead(Presencia);
-   if (val == HIGH and Pres == LOW)   //si está activado
+  val = digitalRead(PRESENCIA);
+   if (val == HIGH and pres == LOW)   //si está activado
    {    
         Serial.println("Sensor activado");
-        Pres = HIGH;
+        pres = HIGH;
       
    } 
   }
 
-void LecturaDHT11() {
+void lecturaDHT11() {
   
   float t=dhtSensor.readTemperature();
   float h=dhtSensor.readHumidity(); 
@@ -110,7 +110,7 @@ void LecturaDHT11() {
 
 void setSubscriptions() {
   subscribe(COMMAND_TOPIC);
-  subscribe(TEST_TOPIC);
+  //subscribe(TEST_TOPIC);
 }
 
 void subscribe(char *newTopic) {
@@ -124,26 +124,26 @@ void subscribe(char *newTopic) {
 void callback(char *topic, byte *payload, unsigned int length) {
   // Register all subscription topics
   static const String cmdTopicStr = createTopic(COMMAND_TOPIC);
-  static const String testTopicStr = createTopic(TEST_TOPIC);
+//static const String testTopicStr = createTopic(TEST_TOPIC);
 
   String msg = unwrapMessage(payload, length);
   Serial.println(" => " + String(topic) + ": " + msg);
 
   // What to do in each topic case?
   if (String(topic) == cmdTopicStr) {
-    if (msg== "true" and Pres == HIGH){
-      digitalWrite(Motor,HIGH);
+    if (msg== "true" and pres == HIGH){
+      digitalWrite(MOTOR,HIGH);
       digitalWrite(LED_VERDE,HIGH);
       digitalWrite(LED_ROJO,LOW);
       digitalWrite(LED_AZUL,LOW);
       delay(2000);
-      Pres=LOW;
-      digitalWrite(Motor,LOW); 
+      pres=LOW;
+      digitalWrite(MOTOR,LOW); 
       }else if (msg=="false"){
-        digitalWrite(Motor,LOW);
-        Pres=LOW;
+        digitalWrite(MOTOR,LOW);
+        pres=LOW;
         }
-  } else if (String(topic) == testTopicStr) {
+  //} else if (String(topic) == testTopicStr) {
     // Do some other stuff
   } else {
     Serial.println("[WARN] - '" + String(topic) +
